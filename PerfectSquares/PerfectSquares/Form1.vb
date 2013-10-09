55174eed1756ca239b03c8d5345f5df7423865c9
Joshua Holt <holt0703@umn.edu>
2013-10-09 15:19:32 -0500

    Got it

--- PerfectSquares/PerfectSquares/Form1.vb
+++ PerfectSquares/PerfectSquares/Form1.vb
@@ -3,7 +3,15 @@
     Private Sub btnDisplay_Click(sender As System.Object, e As System.EventArgs) Handles btnDisplay.Click
 
         lstResult.Items.Clear()
-        lstResult.Items.Add("Not implemented yet")
+
+        Dim i As Integer = 1
+
+        Do While (i <= 10)
+            Dim x As Integer = i ^ 2
+            lstResult.Items.Add(x)
+            i += 1
+        Loop
+
 
     End Sub
 End Class
